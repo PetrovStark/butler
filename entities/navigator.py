c37c7e8e177a8664b7b2905d7186b9fc0d74d64c
Petrus Nogueira <petrusyagami@gmail.com>
2024-05-10 01:24:11 -0300

    feat: login and fill datacompetencia

--- entities/navigator.py
+++ entities/navigator.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 from selenium import webdriver
 import os
 import json
@@ -19,11 +20,15 @@ class Navigator:
         })
         self.cursor = webdriver.Chrome(service=driver, chrome_options=self.options)
         self.action = ActionChains(driver)
+        self.by = By
+        self.fields = []
     
     def __create_download_folder(self):
         folder_name = f'{os.getcwd()}/downloads/'
-        
         if not os.path.exists(folder_name):
-            os.makedirs(folder_name, exist_ok=True)
-        
+            os.makedirs(folder_name, exist_ok=True)  
         return folder_name
+    
+    def fillFormByCSSSelector(self):
+        for field in self.fields:
+            self.cursor.find_element(self.by.CSS_SELECTOR, f"input[name='{field[0]}']").send_keys(field[1])
--- entities/nfse.py
+++ entities/nfse.py
@@ -1,13 +1,56 @@
 from entities.navigator import Navigator
+from datetime import datetime
+import json
+import time
 
 class Nfse(Navigator):
     def __init__(self):
         Navigator.__init__(self)
 
+        self.data = json.load(open("nfse.json"))
+        self.emission = self.data["emission"]
+        self.tomador = self.data["tomador"]
         self.urls = {
-            "nfse": "https://www.nfse.gov.br/EmissorNacional/"
+            "login_url": "https://www.nfse.gov.br/EmissorNacional/",
+            "create_url": "https://www.nfse.gov.br/EmissorNacional/DPS/Pessoas"
         }
+        self.fields = {}
     
+    def __getDataCompetencia(self):
+        if not self.emission["day"]:
+            self.emission["day"] = 1
+        if bool(self.emission["custom"]):
+            custom_date = datetime(
+                self.emission["year"],
+                self.emission["month"],
+                self.emission["day"]
+            )
+        else :
+            custom_date = datetime(
+                datetime.now().year,
+                datetime.now().month,
+                self.emission["day"]
+            )
+        return custom_date.strftime('%d-%m-%Y')
+
     def download(self):
-        self.cursor.get(self.urls['nfse'])
-        self.action.pause(50000)
+        self.login()
+        self.create()
+    
+    def login(self):
+        self.fields = [
+            ["Inscricao", self.data["cnpj"]],
+            ["Senha", self.data["password"]]
+        ]
+        self.cursor.get(self.urls['login_url'])
+        self.fillFormByCSSSelector()
+        self.cursor.find_element(self.by.CSS_SELECTOR, 'button[type="submit"]').click()
+        self.fields = {}
+    
+    def create(self):
+        self.fields = [
+            ["DataCompetencia", self.__getDataCompetencia()]
+        ]
+        self.cursor.get(self.urls['create_url'])
+        self.fillFormByCSSSelector()
+        time.sleep(10)
